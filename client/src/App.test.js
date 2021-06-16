97cff1abd0739fc2ddf7462c3d6f9f3aecfdff69
KasparasKralikas <kasparaskralikas@gmail.com>
2021-06-16 23:06:35 +0300

    Revert "Boilerplate for jest unit tests"
    This reverts commit 65b4b6f895d5acc5eccca241359eea8ff1721e84.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,1 +1,8 @@
-test.todo('App is rendering with expected contents');
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- server/tests/tableManager.test.js
+++
@@ -1,5 +0,0 @@
-TableManger = require('../tableManager.js');
-
-describe('TableManager creation tests', () => {
-    test.todo('Should create TableManager instance with default parameters');
-});
